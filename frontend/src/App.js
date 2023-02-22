0c14b82de7b215018e977ed3c157a0dbe7ef8cef
Rii-C <rrishi535@gmail.com>
2023-02-22 12:40:47 +0530

    add-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './components/AllRoutes'
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-   <AllRoutes/>
+   {/* <AllRoutes/> */}
+   <Navbar/>
     </div>
   );
 }
